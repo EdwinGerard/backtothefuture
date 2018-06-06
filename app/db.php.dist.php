adb9bf85cc54994fde472d8126c3b7fcbf4d9645
Edwin Gerard <edwin.gerard1@gmail.com>
2018-06-06 10:40:17 +0200

    Ajout fichier db.php.dist

---
+++ app/db.php.dist.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aragorn
+ * Date: 06/06/18
+ * Time: 10:38
+ */
+
+
+/**
+ * This file define BD access infos.
+ *
+ */
+
+define('APP_DB_USER', 'user');
+define('APP_DB_PWD', 'password');
+define('APP_DB_HOST', 'localhost');
+define('APP_DB_NAME', 'database_name');
